4bbb6c3f4fe1687deb072d692d769981de08d816
Prasad <prasad.gawade9@gmail.com>
2022-10-09 15:09:41 +0530

    Deploy

---
+++ src/screens/AddDriverScreen.js
@@ -0,0 +1,179 @@
+import React, { Component } from "react";
+import ManagerApiService from "./../Service/ManagerApiService";
+import Header from "../components/Header";
+import ManagerNavigation from "../components/ManagerNavigation";
+import swal from "sweetalert";
+
+class AddDriverScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      mobile: "",
+      gender: "",
+      dob: "",
+      user: localStorage.getItem("user"),
+    };
+    this.saveDriver = this.saveDriver.bind(this);
+  }
+  saveDriver = (e) => {
+    e.preventDefault();
+    const driver = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      mobile: this.state.mobile,
+      gender: this.state.gender,
+      dob: this.state.dob,
+      role: "DRIVER",
+    };
+    ManagerApiService.addDriver(driver).then((res) => {
+      alert("Driver added successfully");
+      this.setState({ message: "Driver added successfully." });
+      this.props.history.push("/manager");
+    });
+  };
+
+  validate = (e) => {
+    var regEx = /^[a-zA-Z\s]+$/;
+    var pattern = /^[6-9]\d{9}$/gi;
+    if (this.state.name === "") {
+      swal("Error", "Please enter Name", "error");
+      return false;
+      // } else if (!isNaN(Name)) {
+      //   sweetalert("Error", "Please enter valid Name", "error");
+      //   return false;
+    } else if (!regEx.test(this.state.name)) {
+      swal("Error", "Please enter characters and space only", "error");
+      return false;
+    } else if (this.state.mobile === "") {
+      swal("Error", "Please enter Mobile number", "error");
+      return false;
+    } else if (
+      !pattern.test(this.state.mobile) ||
+      isNaN(this.state.mobile) ||
+      this.state.mobile.length <= 9 ||
+      this.state.mobile.length >= 11
+    ) {
+      swal("Error", "Please enter valid Mobile number", "error");
+      return false;
+    } else if (this.state.email === "") {
+      swal("Error", "Please enter email", "error");
+      return false;
+    } else if (this.state.email.indexOf("@") <= 0) {
+      swal("Error", "Please enter valid email", "error");
+      return false;
+    } else if (
+      this.state.email.charAt(this.state.email.length - 4) !== "." &&
+      this.state.email.charAt(this.state.email.length - 3) !== "."
+    ) {
+      swal("Error", "Please enter valid email", "error");
+      return false;
+    } else if (this.state.password === "") {
+      swal("Error", "Please enter password", "error");
+      return false;
+    } else if (this.state.password.length <= 5) {
+      // sweetalert("Error", "Please enter Strong password", "error");
+      swal("Error", "Password must be atleast 6 character", "error");
+
+      return false;
+    }
+    this.saveDriver(e);
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div>
+        {this.state.user === null ? (
+          this.props.history.push("/signin")
+        ) : (
+          <div>
+            <ManagerNavigation />
+            <Header title="Add Driver" />
+            <div>
+              <form className="container">
+                <div className="form-group mb-2 mt-2">
+                  <label>Driver Name:</label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    className="form-control"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className="form-group mb-2 mt-2">
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    className="form-control"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className="form-group mb-2 mt-2">
+                  <label>Password:</label>
+                  <input
+                    placeholder="Password"
+                    name="password"
+                    className="form-control"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className="form-group mb-2 mt-2">
+                  <label>Mobile:</label>
+                  <input
+                    type="number"
+                    placeholder="Mobile"
+                    name="mobile"
+                    className="form-control"
+                    value={this.state.mobile}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className="form-group mb-2 mt-2">
+                  <label>Gender:</label>
+                  <select name="gender" onChange={this.onChange}>
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+                    <option value="O">Other</option>
+                  </select>
+                </div>
+
+                <div className="form-group mb-2 mt-2">
+                  <label>Birth Date:</label>
+                  <input
+                    type="date"
+                    placeholder="Birth Date"
+                    name="dob"
+                    className="form-control"
+                    value={this.state.dob}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <button className="btn btn-success" onClick={this.validate}>
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default AddDriverScreen;
---
+++ src/screens/ShowBookingScreenClass.js
@@ -0,0 +1,151 @@
+import React from "react";
+import UserNavigation from "../components/UserNavigation";
+import BookingApiService from "../Service/BookingApiService";
+import Header from "./../components/Header";
+import swal from "sweetalert";
+
+/*export default class ShowBookingScreenClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: localStorage.getItem("userId"),
+      bookings: [],
+      message: null,
+      user: localStorage.getItem('user')
+    };
+    this.loadBookings = this.loadBookings.bind(this);
+    this.cancelBooking = this.cancelBooking.bind(this);
+    
+  }
+
+  componentWillMount() {
+    console.log(`component did mount`);
+    console.log(this.state.userId);
+    this.loadBookings();
+    
+  }
+
+  loadBookings(){
+
+    BookingApiService.showBookings(this.state.userId)
+    .then((response) => {
+      this.setState({
+        bookings: response.data,
+      });
+      console.log(this.state.bookings);
+    })
+    .catch((error) => {
+      console.log(`Error : ${error}`);
+    });
+  }
+
+  cancelBooking(e) {
+    console.log("in cancel" +e);
+    //const bookID = localStorage.getItem("bookId");
+    BookingApiService.cancelBooking(window.localStorage.getItem("bookId")).then(
+      (res) => {
+        this.setState({ message: "Booking Cancelled successfully." });
+        swal("Booking Cancelled successfully","success");
+        this.props.history.push("/my-bookings");
+      }
+    )
+  }*/
+
+export default class ShowBookingScreenClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: localStorage.getItem("userId"),
+      bookings: [],
+      message: null,
+      user: localStorage.getItem('user')
+    };
+    this.cancelBooking = this.cancelBooking.bind(this);
+
+  }
+
+  componentWillMount() {
+    console.log(`component did mount`);
+    console.log(this.state.userId);
+    BookingApiService.showBookings(this.state.userId)
+      .then((response) => {
+        this.setState({
+          bookings: response.data,
+        });
+        console.log(this.state.bookings);
+      })
+      .catch((error) => {
+        console.log(`Error : ${error}`);
+      });
+  }
+
+  cancelBooking(e) {
+    //const bookID = localStorage.getItem("bookId");
+    BookingApiService.cancelBooking(e).then(
+      (res) => {
+        this.setState({ message: "Booking Cancelled successfully." });
+        swal("Booking Cancelled successfully", "success");
+        this.props.history.push("/view-bus");
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.user === null ?
+            this.props.history.push("/signin")
+            :
+            <div>
+              <UserNavigation />
+              <Header title="My Bookings" />
+              <div>
+                {this.state.bookings.length > 0 && (
+                  <table class="table table-hover m-3" style={{ width: "95vw", marginLeft: "100px", fontSize: "20px" }}>
+                    <thead>
+                      <tr>
+
+                        <th scope="col">From</th>
+                        <th scope="col">To</th>
+                        <th scope="col">Boarding</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Bus Name</th>
+                        <th scope="col">Bus Type</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.bookings.map((booking) => {
+                        return (
+                          <tr>
+
+                            <td>{booking.route.source}</td>
+                            <td>{booking.route.destination}</td>
+                            <td>{booking.seatNumber}</td>
+                            <td>{booking.dateOfJourney}</td>
+                            <td>{booking.bus.busName}</td>
+                            <td>{booking.bus.busType}</td>
+                            <td>
+                              <button
+                                type="button"
+                                class="btn btn-danger btn-lg"
+                                onClick={() => this.cancelBooking(booking.id)}
+                              >
+                                Cancel
+                              </button>
+
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                )}
+              </div>
+            </div>
+        }
+      </div>
+    );
+  }
+}
